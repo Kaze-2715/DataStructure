b50ab024b18b1a2e4e583871eccc4c9255a516db
Kaze <zhaofangming@mail.nwpu.edu.cn>
2025-04-01 23:04:31 +0800

    二叉树的定义和简单函数

---
+++ binaryTree/binaryTree.c
@@ -0,0 +1,171 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binaryTree.h"
+
+void preOrder(Node *node);
+void inOrder(Node *node);
+void postOrder(Node *node);
+
+// TODO: Implement destroy function
+void destroy(Tree *tree)
+{
+}
+
+// TODO: Implement parent function
+Node *parent(Tree *tree, Node *node)
+{
+
+}
+
+// TODO: Implement getLeftChild function
+Node *getLeftChild(Node *node)
+{
+    return node->leftChild;
+}
+
+// TODO: Implement getRightChild function
+Node *getRightChild(Node *node)
+{
+    return node->rightChild;
+}
+
+// TODO: Implement clear function
+void clear(Tree *tree)
+{
+}
+
+// TODO: Implement insertLeftChild function
+Node *insertLeftChild(Node *node, item data)
+{
+    if (node == NULL)
+    {
+        return NULL;
+    }
+    
+    Node *new = malloc(sizeof(Node));
+    new->data = data;
+    new->leftChild = NULL;
+    new->rightChild = NULL;
+    node->leftChild = new;
+    return new;
+}
+
+// TODO: Implement insertRightChild function
+Node *insertRightChild(Node *node, item data)
+{
+    if (node == NULL)
+    {
+        return NULL;
+    }
+
+    Node *new = malloc(sizeof(Node));
+    new->data = data;
+    new->leftChild = NULL;
+    new->rightChild = NULL;
+    node->rightChild = new;
+    return new;
+}
+
+// TODO: Implement Traverse function
+void Traverse(Tree *tree)
+{
+}
+
+// TODO: Implement searchNode function
+Node *searchNode(Tree *tree, Node *node)
+{
+    return NULL;
+}
+
+// TODO: Implement getDepth function
+int getDepth(Tree *tree)
+{
+    return 0;
+}
+
+// TODO: Implement getNodeCount function
+int getNodeCount(Tree *tree)
+{
+    return 0;
+}
+
+// TODO: Implement isEmpty function
+int isEmpty(Tree *tree)
+{
+    return (tree->root == NULL) ? 1 : 0;
+}
+
+// TODO: Implement getLeafCount function
+int getLeafCount(Tree *tree)
+{
+    return 0;
+}
+
+// TODO: Implement preOrderTraverse function
+void preOrderTraverse(Tree *tree)
+{
+}
+
+// TODO: Implement inOrderTraverse function
+void inOrderTraverse(Tree *tree)
+{
+}
+
+// TODO: Implement postOrderTraverse function
+void postOrderTraverse(Tree *tree)
+{
+}
+
+// TODO: Implement levelOrderTraverse function
+void levelOrderTraverse(Tree *tree)
+{
+}
+
+// TODO: Implement mirror function
+void mirror(Tree *tree)
+{
+}
+
+// TODO: Implement isComplete function
+int isComplete(Tree *tree)
+{
+    return 0;
+}
+
+// TODO: Implement getWidth function
+int getWidth(Tree *tree)
+{
+    return 0;
+}
+
+// TODO: Implement findLCA function
+Node *findLCA(Tree *tree, Node *node1, Node *node2)
+{
+    return NULL;
+}
+
+void initTree(Tree *tree)
+{
+    tree->root = NULL;
+    tree->destroy = destroy;
+    tree->parent = parent;
+    tree->getLeftChild = getLeftChild;
+    tree->getRightChild = getRightChild;
+    tree->clear = clear;
+    tree->insertLeftChild = insertLeftChild;
+    tree->insertRightChild = insertRightChild;
+    tree->Traverse = Traverse;
+    tree->searchNode = searchNode;
+    tree->getDepth = getDepth;
+    tree->getNodeCount = getNodeCount;
+    tree->isEmpty = isEmpty;
+    tree->getLeafCount = getLeafCount;
+    tree->preOrderTraverse = preOrderTraverse;
+    tree->inOrderTraverse = inOrderTraverse;
+    tree->postOrderTraverse = postOrderTraverse;
+    tree->levelOrderTraverse = levelOrderTraverse;
+    tree->mirror = mirror;
+    tree->isComplete = isComplete;
+    tree->getWidth = getWidth;
+    tree->findLCA = findLCA;
+}
--- binaryTree/binaryTree.h
+++ binaryTree/binaryTree.h
@@ -1,1 +1,80 @@
-typedef struct binaryTree tree;
+typedef struct binaryTree Tree;
+typedef struct treeNode Node;
+typedef int item;
+
+// 数据结构定义
+struct treeNode
+{
+    item data;
+    Node *leftChild;
+    Node *rightChild;
+};
+
+struct binaryTree
+{
+    Node *root;
+
+    // 基本操作
+    void (*destroy)(Tree *tree);
+    void (*clear)(Tree *tree);
+
+    // 节点操作
+    Node *(*insertLeftChild)(Node *node, item data);
+    Node *(*insertRightChild)(Node *node, item data);
+    Node *(*searchNode)(Tree *tree, Node *node);
+    Node *(*parent)(Tree *tree, Node *node);
+    Node *(*getLeftChild)(Node *node);
+    Node *(*getRightChild)(Node *node);
+
+    // 遍历操作
+    void (*Traverse)(Tree *tree);
+    void (*preOrderTraverse)(Tree *tree);
+    void (*inOrderTraverse)(Tree *tree);
+    void (*postOrderTraverse)(Tree *tree);
+    void (*levelOrderTraverse)(Tree *tree);
+
+    // 树的属性查询
+    int (*getDepth)(Tree *tree);
+    int (*getNodeCount)(Tree *tree);
+    int (*isEmpty)(Tree *tree);
+    int (*getLeafCount)(Tree *tree);
+    int (*getWidth)(Tree *tree);
+
+    // 高级操作
+    void (*mirror)(Tree *tree);
+    int (*isComplete)(Tree *tree);
+    Node *(*findLCA)(Tree *tree, Node *node1, Node *node2);
+};
+
+// 函数声明
+// 基本操作
+void initTree(Tree *tree);
+void destroy(Tree *tree);
+void clear(Tree *tree);
+
+// 节点操作
+Node *insertLeftChild(Node *node, item data);
+Node *insertRightChild(Node *node, item data);
+Node *searchNode(Tree *tree, Node *node);
+Node *parent(Tree *tree, Node *node);
+Node *getLeftChild(Node *node);
+Node *getRightChild(Node *node);
+
+// 遍历操作
+void Traverse(Tree *tree);
+void preOrderTraverse(Tree *tree);
+void inOrderTraverse(Tree *tree);
+void postOrderTraverse(Tree *tree);
+void levelOrderTraverse(Tree *tree);
+
+// 树的属性查询
+int getDepth(Tree *tree);
+int getNodeCount(Tree *tree);
+int isEmpty(Tree *tree);
+int getLeafCount(Tree *tree);
+int getWidth(Tree *tree);
+
+// 高级操作
+void mirror(Tree *tree);
+int isComplete(Tree *tree);
+Node *findLCA(Tree *tree, Node *node1, Node *node2);
